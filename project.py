5cc83f4a51dcbcddbc5df6f55b5e5007f4f971db
S.M.ABRAR MUSTAKIM TAKI <79537159+Abrar-Mustakim@users.noreply.github.com>
2023-08-14 11:37:33 +0600

    Add files via upload

--- project.py
+++ project.py
@@ -4,9 +4,7 @@ from OpenGL.GLU import *
 from math import cos, sin
 import random
 
-
-
-#Midpoint Line Drawing Algorithms
+# Midpoint Line Drawing Algorithms
 '''
 def midpointcircle(x, y, r, color):
     glColor3f(*color) # Set the color
@@ -67,14 +65,14 @@ def midpointcircle(x, y, r, color):
 '''
 
 
-# Mid point line drawing algo 
+# Mid point line drawing algo
 def drawPoint(x, y):
     glPointSize(20)
     glBegin(GL_POINTS)
     glVertex2f(x, y)
     glEnd()
-    
-    
+
+
 def findZone(x1, y1, x2, y2):
     dx = x2 - x1
     dy = y2 - y1
@@ -125,7 +123,6 @@ def midPointAlgo(x1, y1, x2, y2):
             d += incE
 
 
-
 def convertToZoneZero(x1, y1, x2, y2, zone):
     zone_mappings = {
         0: (x1, y1, x2, y2),
@@ -139,7 +136,6 @@ def convertToZoneZero(x1, y1, x2, y2, zone):
     }
 
     return zone_mappings[zone]
-
 
 
 def convertFromZoneZeroToZoneSmth(x, y, zone):
@@ -157,7 +153,7 @@ def convertFromZoneZeroToZoneSmth(x, y, zone):
     return zone_mappings[zone]
 
 
-#Midpoint Circle Drawing Algorithm
+# Midpoint Circle Drawing Algorithm
 def midpointcircle(x, y, r, color):
     glColor3f(*color)
     glPointSize(2)  # pixel size
@@ -184,7 +180,10 @@ def midpointcircle(x, y, r, color):
             glVertex2f(fill_x, -x + y1)
     glEnd()
 
+
+# We have used this because our midpoint algorithm seems slow for the UI
 #We have used this because our midpoint algorithm seems slow for the UI
+sun_scale = 1.0  # Global variable to control the scaling of the sun
 def draw_circle(x_center, y_center, radius, color):
     if len(color) == 3:
         glColor3f(*color) # Set the color
@@ -200,33 +199,50 @@ def draw_circle(x_center, y_center, radius, color):
     glEnd()
 
 
-def draw_treeX(x,y):
-    glColor3f(0.5, 0.3, 0.0)
-    midPointAlgo(x+10,y+50,x+10,y-70)
-    midPointAlgo(x-10,y+50,x-10,y-70)
-    midPointAlgo(x+10,y-70,x-10,y-70)
-    tree_leaf(x,y+50)
+def draw_circle_X(x_center, y_center, radius, color):
+    if len(color) == 3:
+        glColor3f(*color) # Set the color
+    elif len(color) ==4:
+        glColor4f(*color)
+    glBegin(GL_POLYGON)
+    num_segments = 100 # Number of segments for smooth circle
+    for i in range(num_segments):
+        theta = 2.0 * 3.1415926 * i / num_segments
+        dx = radius * sun_scale * cos(theta)
+        dy = radius * sun_scale * sin(theta)
+        glVertex2f(dx + x_center, dy + y_center)
+    glEnd()
 
-def tree_leaf(x,y):
+def draw_treeX(x, y):
+    glColor3f(0.5, 0.3, 0.0)
+    midPointAlgo(x + 10, y + 50, x + 10, y - 70)
+    midPointAlgo(x - 10, y + 50, x - 10, y - 70)
+    midPointAlgo(x + 10, y - 70, x - 10, y - 70)
+    tree_leaf(x, y + 50)
+
+
+def tree_leaf(x, y):
     # midpointcircle(x, y, 50, (1, 1, 1))
     # midpointcircle(x + 50, y, 40, (1, 1, 1))
     # midpointcircle(x - 50, y, 40, (1, 1, 1))
-    draw_circle(x, y, 60, (0,1,0))
+    draw_circle(x, y, 60, (0, 1, 0))
     draw_circle(x + 50, y, 40, (0, 1, 0))
     draw_circle(x - 50, y, 30, (0, 1, 0))
-    draw_circle(x + 50, y-50, 20, (0, 1, 0))
-    draw_circle(x - 50, y+20, 30, (0, 1, 0))
-    draw_circle(x + 50, y+10, 30, (0, 1, 0))
-    draw_circle(x - 50, y+20, 30, (0, 1, 0))
-    draw_circle(x + 50, y+10, 30, (0, 1, 0))
+    draw_circle(x + 50, y - 50, 20, (0, 1, 0))
+    draw_circle(x - 50, y + 20, 30, (0, 1, 0))
+    draw_circle(x + 50, y + 10, 30, (0, 1, 0))
+    draw_circle(x - 50, y + 20, 30, (0, 1, 0))
+    draw_circle(x + 50, y + 10, 30, (0, 1, 0))
     draw_circle(x - 50, y, 30, (0, 1, 0))
-    draw_circle(x - 40, y-40, 30, (0, 1, 0))
-    draw_circle(x + 30, y+40, 15, (0, 1, 0))
+    draw_circle(x - 40, y - 40, 30, (0, 1, 0))
+    draw_circle(x + 30, y + 40, 15, (0, 1, 0))
+
 
 rain_animation = False
 rain_timer = 0
 rain_duration = 120  # 5 seconds at 20 FPS
 raindrops = [(random.uniform(0, 800), random.uniform(0, 800)) for _ in range(2000)]
+
 
 def draw_raindrop(x, y):
     glColor3f(0.5, 0.5, 1.0)
@@ -235,8 +251,11 @@ def draw_raindrop(x, y):
     glVertex2f(x, y + 3)
     glEnd()
 
+
 # Function to draw sky (gradient background)
 is_day = True
+
+
 def draw_sky():
     glBegin(GL_QUADS)
     if is_day and not rain_animation:  # Day without rain
@@ -258,7 +277,8 @@ def draw_sky():
     glVertex2f(0, 300)
     glEnd()
 
-#Clouds
+
+# Clouds
 def draw_cloud(x, y):
     if rain_animation:
         glColor3f(0.5, 0.5, 0.5)  # Dark grey clouds during rain
@@ -270,8 +290,9 @@ def draw_cloud(x, y):
     draw_circle(x, y, 50, (1, 1, 1))
     draw_circle(x + 50, y, 40, (1, 1, 1))
     draw_circle(x - 50, y, 40, (1, 1, 1))
-    
-#Stars
+
+
+# Stars
 def draw_star(x, y):
     # Drawing a simple star using a point
     glColor3f(1, 1, 1)  # White color for stars
@@ -279,6 +300,8 @@ def draw_star(x, y):
     glBegin(GL_POINTS)
     glVertex2f(x, y)
     glEnd()
+
+
 star_positions = [(random.uniform(0, 800), random.uniform(800, 400)) for _ in range(100)]
 
 
@@ -292,10 +315,11 @@ def draw_ground():
     glVertex2f(0, 0)
     glEnd()
 
+
 # Function to draw a simple house
 def draw_house(situation):
     # Draw main body of the house
-    glColor4f(1.0, 0.5, 0.0, 0.0)# Brown color for the house
+    glColor4f(1.0, 0.5, 0.0, 0.0)  # Brown color for the house
     glBegin(GL_POLYGON)
     glVertex2d(200, 200)
     glVertex2d(300, 200)
@@ -303,7 +327,6 @@ def draw_house(situation):
     glVertex2d(250, 300)
     glVertex2d(200, 250)
     glVertex2d(200, 200)
-
 
     glVertex2d(300, 200)
     glVertex2d(350, 200)
@@ -331,7 +354,6 @@ def draw_house(situation):
     glVertex2d(300, 250)
     glEnd()
 
-    
     glColor3f(0.5, 0.0, 0.0)  # Black color for windows
     glBegin(GL_POLYGON)
     glVertex2d(310, 270)
@@ -341,13 +363,12 @@ def draw_house(situation):
     glVertex2d(310, 270)
     glEnd()
 
-
     # Draw windows
     # Window 1
     if situation == "day":
         glColor3f(0.0, 0.0, 0.0)  # Black color for windows
     else:
-        glColor3f(1.0, 1.0, 1.0) 
+        glColor3f(1.0, 1.0, 1.0)
     glBegin(GL_QUADS)
     glVertex2d(310, 220)
     glVertex2d(340, 220)
@@ -355,11 +376,10 @@ def draw_house(situation):
     glVertex2d(310, 240)
     glEnd()
 
-
     if situation == "day":
         glColor3f(0.0, 0.0, 0.0)  # Black color for windows
     else:
-        glColor3f(1.0, 1.0, 1.0) 
+        glColor3f(1.0, 1.0, 1.0)
     glBegin(GL_QUADS)
     glVertex2d(210, 220)
     glVertex2d(230, 220)
@@ -369,14 +389,14 @@ def draw_house(situation):
     if situation == "day":
         glColor3f(0.0, 0.0, 0.0)  # Black color for windows
     else:
-        glColor3f(1.0, 1.0, 1.0) 
+        glColor3f(1.0, 1.0, 1.0)
     glBegin(GL_QUADS)
     glVertex2d(275, 220)
     glVertex2d(295, 220)
     glVertex2d(295, 240)
     glVertex2d(275, 240)
     glEnd()
-   
+
     # Door
     glColor3f(0.0, 0.5, 0.5)  # Black color for windows
     glBegin(GL_QUADS)
@@ -386,13 +406,13 @@ def draw_house(situation):
     glVertex2d(265, 200)
     glEnd()
 
-    #Door Nob
+    # Door Nob
     glColor3f(1.0, 1.0, 0.5)  # Black color for windows
     glPointSize(5)
     glBegin(GL_POINTS)
     glVertex2f(260, 220)
     glEnd()
-    
+
 
 # Function to draw a simple tree
 def draw_tree(x, y):
@@ -414,6 +434,7 @@ def draw_tree(x, y):
         glVertex2f(x + 0.025, y + 0.25 + i * 0.05)
         glEnd()
 
+
 def draw_bird(x, y):
     glColor3f(0, 0, 0)  # Black color for the bird
     glBegin(GL_LINES)
@@ -424,16 +445,16 @@ def draw_bird(x, y):
     glEnd()
 
 
-
 # Global variable to hold bird positions
 bird_positions = [
-    (600, 400), (700, 450), (550, 550), 
+    (600, 400), (700, 450), (550, 550),
     (700, 300), (750, 400), (750, 550),
     (800, 450), (900, 500),
     (600, 300), (500, 500)
-] 
+]
 # Global variable to control bird animation
 bird_animation = False
+
 
 def animate_birds():
     global bird_positions
@@ -450,6 +471,7 @@ def draw_river():
     glVertex2f(500, 300)
     glEnd()
 
+
 # Reflection Transformation Function to draw the reflection of the boat
 def draw_boat_reflection(x_position, is_day):
     if not is_day:
@@ -459,7 +481,7 @@ def draw_boat_reflection(x_position, is_day):
     reflection_y = 200  # Level of the river where the reflection occurs
 
     # Drawing the base reflection with reduced opacity and slight color modification
-    #glColor4f(0.6, 0.3, 0.1, 0.2)  # Slightly lighter and transparent color
+    # glColor4f(0.6, 0.3, 0.1, 0.2)  # Slightly lighter and transparent color
     glColor4f(0.6, 0.3, 0.1, 0.05)
     glBegin(GL_POLYGON)
     glVertex2f(x_position, 2 * reflection_y - 270)
@@ -470,8 +492,8 @@ def draw_boat_reflection(x_position, is_day):
     glEnd()
 
     # Drawing the upper part reflection with reduced opacity and slight color modification
-    #glColor4f(0.2, 0.8, 0.2, 0.3)  # Slightly lighter and transparent color
-    #glColor4f(0.3, 0.75, 0.7, 0.2)
+    # glColor4f(0.2, 0.8, 0.2, 0.3)  # Slightly lighter and transparent color
+    # glColor4f(0.3, 0.75, 0.7, 0.2)
     glColor4f(0.3, 0.75, 0.7, 0.1)
     glBegin(GL_POLYGON)
     glVertex2f(x_position + 75, 2 * reflection_y - 290)
@@ -482,8 +504,8 @@ def draw_boat_reflection(x_position, is_day):
     glEnd()
 
     # Additional reflection details
-    #glColor4f(0.2, 0.2, 0.8, 0.3)  # Slightly lighter and transparent color
-    #glColor4f(0.3, 0.6, 0.9, 0.2)
+    # glColor4f(0.2, 0.2, 0.8, 0.3)  # Slightly lighter and transparent color
+    # glColor4f(0.3, 0.6, 0.9, 0.2)
     glColor4f(0.3, 0.6, 0.9, 0.05)
     glBegin(GL_QUADS)
     glVertex2f(x_position + 75, 2 * reflection_y - 270)
@@ -492,50 +514,49 @@ def draw_boat_reflection(x_position, is_day):
     glVertex2f(x_position + 72, 2 * reflection_y - 270)
     glEnd()
 
-    draw_circle(x_position+113, 307-reflection_y-15, 13, (1.0, 0.3, 0.3, 0.05))
-
-
+    draw_circle(x_position + 113, 307 - reflection_y - 15, 13, (1.0, 0.3, 0.3, 0.05))
 
 
 def draw_boat(x_position, is_day):
     # Draw the base of the boat
     glColor3f(0.5, 0.25, 0.0)  # Brown color
     glBegin(GL_POLYGON)
-    #glVertex2f(x_position - 0.2, -0.2)
-    #glVertex2f(x_position + 0.2, -0.2)
-    #glVertex2f(x_position + 0.2, -0.15)
-    #glVertex2f(x_position - 0.2, -0.15)
+    # glVertex2f(x_position - 0.2, -0.2)
+    # glVertex2f(x_position + 0.2, -0.2)
+    # glVertex2f(x_position + 0.2, -0.15)
+    # glVertex2f(x_position - 0.2, -0.15)
     glVertex2f(x_position, 270)
-    glVertex2f(x_position+45, 220)
-    glVertex2f(x_position+110, 220)
-    glVertex2f(x_position+150, 270)
+    glVertex2f(x_position + 45, 220)
+    glVertex2f(x_position + 110, 220)
+    glVertex2f(x_position + 150, 270)
     glVertex2f(x_position, 270)
-    
+
     glEnd()
 
     # Draw the upper part of the boat
     glColor3f(0.0, 0.9, 0.0)  # Slightly darker shade for the upper part
     glBegin(GL_POLYGON)
-    glVertex2f(x_position+75, 290)
-    glVertex2f(x_position+75, 330)
-    glVertex2f(x_position+150, 330)
-    glVertex2f(x_position+150, 290)
-    glVertex2f(x_position+75, 290)
+    glVertex2f(x_position + 75, 290)
+    glVertex2f(x_position + 75, 330)
+    glVertex2f(x_position + 150, 330)
+    glVertex2f(x_position + 150, 290)
+    glVertex2f(x_position + 75, 290)
     glEnd()
 
-    glColor3f(0.0, 0.0, 0.9)  # Blue stick of the flag
+    glColor3f(0.0, 0.0, 0.9)  # Green color for the ground
     glBegin(GL_QUADS)
-    glVertex2f(x_position+ 75, 270)
-    glVertex2f(x_position+ 75, 330)
-    glVertex2f(x_position+ 72, 330)
-    glVertex2f(x_position+ 72, 270)
+    glVertex2f(x_position + 75, 270)
+    glVertex2f(x_position + 75, 330)
+    glVertex2f(x_position + 72, 330)
+    glVertex2f(x_position + 72, 270)
     glEnd()
 
     # Green color for the ground
-    
-    draw_circle(x_position+113, 307, 13, (1, 0, 0))
+
+    draw_circle(x_position + 113, 307, 13, (1, 0, 0))
 
     draw_boat_reflection(x_position, is_day)
+
 
 boat_position = 550
 
@@ -544,15 +565,16 @@ boat_position = 550
 def draw_flower(x, y):
     # Draw petals using circles
     petal_radius = 10
-    petal_color = (0.4, 0.0, 0.0) # Red color
+    petal_color = (0.4, 0.0, 0.0)  # Red color
     for angle in range(0, 360, 60):
         petal_x = x + petal_radius * cos(angle * 3.14 / 180)
         petal_y = y + petal_radius * sin(angle * 3.14 / 180)
-        draw_circle(petal_x, petal_y, petal_radius, petal_color)
+        draw_circle_X(petal_x, petal_y, petal_radius, petal_color)
     # Draw center of the flower using a circle
     center_radius = 5
-    center_color = (1.0, 1.0, 0.0) # Yellow color
-    draw_circle(x, y, center_radius, center_color)
+    center_color = (1.0, 1.0, 0.0)  # Yellow color
+    draw_circle_X(x, y, center_radius, center_color)
+
 
 # Function to draw the garden with flowers
 def draw_garden():
@@ -561,8 +583,9 @@ def draw_garden():
         for y in range(50, 200, 50):
             draw_flower(x, y)
 
-def draw_fruit(x, y, color=(1.0, 0.0, 0.0)): # Default color is red
-    fruit_radius = 5 # You can adjust the size of the fruit
+
+def draw_fruit(x, y, color=(1.0, 0.0, 0.0)):  # Default color is red
+    fruit_radius = 5  # You can adjust the size of the fruit
     midpointcircle(x, y, fruit_radius, color)
 
 
@@ -571,26 +594,25 @@ def iterate():
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     glOrtho(0.0, 800, 0.0, 800, 0.0, 1.0)
-    glMatrixMode (GL_MODELVIEW)
+    glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
 
-    
 def showScreen():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
     iterate()
     global is_day, rain_animation, rain_timer, boat_position
-     # Draw sky and ground
+    # Draw sky and ground
     draw_sky()
     draw_ground()
     # Draw houses
     draw_house("day")
     global bird_animation
     draw_river()
-    #draw_boat(boat_position)
-    draw_treeX(70,250)
-    draw_treeX(420,280)
+    # draw_boat(boat_position)
+    draw_treeX(70, 250)
+    draw_treeX(420, 280)
     draw_garden()
     draw_fruit(50, 270, color=(1.0, 0.0, 0.0))
     draw_fruit(60, 280, color=(1.0, 0.0, 0.0))
@@ -601,7 +623,7 @@ def showScreen():
     global rain_timer, rain_animation
     # Rain Scene
     if rain_animation:
-        #glColor3f(0.9, 0.9, 0.9)  # Grey clouds during rain
+        # glColor3f(0.9, 0.9, 0.9)  # Grey clouds during rain
         draw_cloud(500, 450)
         draw_cloud(300, 420)
         draw_cloud(200, 500)
@@ -619,7 +641,7 @@ def showScreen():
             raindrops = [(random.uniform(0, 800), random.uniform(0, 800)) for _ in range(2000)]
     # Day Scene
     elif is_day:
-        #draw_circle(700, 500, 40, (1.0, 0.843, 0.0))  # Sun
+        # draw_circle(700, 500, 40, (1.0, 0.843, 0.0))  # Sun
         midpointcircle(700, 500, 40, (1.0, 0.843, 0.0))
         draw_cloud(500, 450)
         draw_cloud(300, 420)
@@ -628,7 +650,7 @@ def showScreen():
         draw_boat(boat_position, is_day)
     # Night Scene
     else:
-        #draw_circle(700, 500, 40, (1, 1, 1))  # Moon
+        # draw_circle(700, 500, 40, (1, 1, 1))  # Moon
         midpointcircle(700, 500, 40, (1, 1, 1))
         for x, y in star_positions:
             draw_star(x, y)
@@ -642,8 +664,9 @@ def showScreen():
             draw_bird(x, y)
     glutSwapBuffers()
 
+
 def keyboard(key, x, y):
-    global is_day, bird_animation
+    global is_day, bird_animation, sun_scale
     global is_day, rain_animation, rain_timer, boat_position
     if key == b'd':
         is_day = not is_day
@@ -656,19 +679,22 @@ def keyboard(key, x, y):
         boat_position += 5
     if key == b'n':
         if boat_position < 500:
-            boat_position -=0
+            boat_position -= 0
         else:
             boat_position -= 5
-    glutPostRedisplay()
 
+    if key == b's' or key == b'S':
+        # Toggle scaling factor between 1.0 and 1.5 when the "S" key is pressed
+        sun_scale = 1.5 if sun_scale == 1.0 else 1.0
+    glutPostRedisplay()
 
 
 glutInit()
 glutInitDisplayMode(GLUT_RGBA)
 glutInitWindowSize(800, 600)
 glutInitWindowPosition(0, 0)
-wind = glutCreateWindow("Village Scenario") #window name
+wind = glutCreateWindow("Village Scenario")  # window name
 glutDisplayFunc(showScreen)
-glutIdleFunc(showScreen) # Keep drawing the scene
+glutIdleFunc(showScreen)  # Keep drawing the scene
 glutKeyboardFunc(keyboard)
 glutMainLoop()
